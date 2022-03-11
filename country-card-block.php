43db9175b37dc8b72099c17f29a2b5a491db2e94
Adam Richards <adam.richards@xwp.co>
2022-03-11 15:18:39 -0600

    Fix coding standards issues

--- country-card-block.php
+++ country-card-block.php
@@ -49,11 +49,11 @@ add_action( 'init', __NAMESPACE__ . '\\disable_emojis' );
  * @return array Difference between the two arrays.
  */
 function remove_emoji_dns_prefetch( $urls, $relation_type ) {
-	if ( 'dns-prefetch' == $relation_type ) {
+	if ( 'dns-prefetch' === $relation_type ) {
 		/** This filter is documented in wp-includes/formatting.php */
 		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
 
-		$urls = array_diff( $urls, [ $emoji_svg_url ] );
+		$urls = array_diff( $urls, array( $emoji_svg_url ) );
 	}
 
 	return $urls;
