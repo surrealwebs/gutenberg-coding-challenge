90ae7f1491f5fe10efb9e6f20854d87f7b57c1af
Adam Richards <adam.richards@xwp.co>
2022-03-25 16:16:43 -0500

    Fix various JS linting issues.

--- src/save.js
+++ src/save.js
@@ -1,14 +1,17 @@
+/**
+ * WordPress dependencies
+ */
 import { useBlockProps } from '@wordpress/block-editor';
+/**
+ * Internal dependencies
+ */
 import Preview from './preview';
 
 export default function Save( { attributes } ) {
-
-	const { countryCode, relatedPosts } = attributes;
-
 	const blockProps = useBlockProps.save();
 
 	return (
-		<div { ...blockProps } >
+		<div { ...blockProps }>
 			<Preview { ...attributes } />
 		</div>
 	);
